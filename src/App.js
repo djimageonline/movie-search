ee3b355f9db23ee93575c8a05dcc521f7c874e19
Johnny Proano <djimageonline@gmail.com>
2023-01-18 00:29:38 -0500

    created const movie1 to test api, created movie card, added temp styling, added icon

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useEffect } from "react";
 import "./App.css";
+import { MovieCard } from "./MovieCard";
 
 //
 export function App() {
@@ -12,6 +13,15 @@ export function App() {
     console.log(data.Search);
   };
 
+  const movie1 = {
+    Title: "Italian Spiderman",
+    Year: "2007",
+    imdbID: "tt2705436",
+    Type: "movie",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+  };
+
   useEffect(() => {
     searchMovies("Spiderman");
   }, []);
@@ -19,6 +29,15 @@ export function App() {
   return (
     <div className="movie-app">
       <h1>World of Movies</h1>
+
+      <div className="search">
+        <input placeholder="Search a movie" value="Superman" onChange={() => {}} />
+        <img src="/icons8-search.svg" alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <MovieCard movie1={movie1} />
+      </div>
     </div>
   );
 }
