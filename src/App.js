fb9e219fe68438b584091a2d9804d8936708534c
Johnny Proano <djimageonline@gmail.com>
2023-01-18 00:02:05 -0500

    started by implementing api and key with search

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
+import React from "react";
+import { useEffect } from "react";
 import "./App.css";
 
-const App = () => {
-  const name = "";
-  const isNameShowing = false;
+//
+export function App() {
+  const API_URL = "http://www.omdbapi.com?apikey=";
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
+
+  useEffect(() => {
+    searchMovies("Spiderman");
+  }, []);
 
   return (
-    <div className="App">
-      <h1>Hello {isNameShowing ? name : "someone"}</h1>
-      {name ? (
-        <>
-          <h1>{name}</h1>
-        </>
-      ) : (
-        <>
-          <p>No name</p>
-        </>
-      )}
+    <div className="movie-app">
+      <h1>World of Movies</h1>
     </div>
   );
-};
-
-export default App;
+}
