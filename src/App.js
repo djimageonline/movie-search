307753cf19a0a062cdeaac160aa846a6c66692c3
Johnny Proano <djimageonline@gmail.com>
2023-01-18 00:48:23 -0500

    added map to map through movies if exist display  and added search functionality to current state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import "./App.css";
 import { MovieCard } from "./MovieCard";
 
 //
 export function App() {
+  const [movies, setMovies] = useState();
+  const [searchWord, setSearchWord] = useState();
+
   const API_URL = "http://www.omdbapi.com?apikey=";
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data.Search);
-  };
-
-  const movie1 = {
-    Title: "Italian Spiderman",
-    Year: "2007",
-    imdbID: "tt2705436",
-    Type: "movie",
-    Poster:
-      "https://m.media-amazon.com/images/M/MV5BYjFhN2RjZTctMzA2Ni00NzE2LWJmYjMtNDAyYTllOTkyMmY3XkEyXkFqcGdeQXVyNTA0OTU0OTQ@._V1_SX300.jpg",
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -31,13 +25,21 @@ export function App() {
       <h1>World of Movies</h1>
 
       <div className="search">
-        <input placeholder="Search a movie" value="Superman" onChange={() => {}} />
-        <img src="/icons8-search.svg" alt="search" onClick={() => {}} />
+        <input placeholder="Search a movie" value={searchWord} onChange={(e) => setSearchWord(e.target.value)} />
+        <img src="/icons8-search.svg" alt="search" onClick={() => searchMovies(searchWord)} />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>Nothing found</h2>
+        </div>
+      )}
     </div>
   );
 }
